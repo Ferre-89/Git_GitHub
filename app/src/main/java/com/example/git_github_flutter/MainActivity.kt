3a00e43b78a06e26579f82746d88448cd0d6cfdb
Ferre-89 <ferre171717@gmail.com>
2023-10-25 14:13:42 -0300

    Se ajusto detalle

--- app/src/main/java/com/example/git_github_flutter/MainActivity.kt
+++ app/src/main/java/com/example/git_github_flutter/MainActivity.kt
@@ -22,7 +22,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Curso GitHub")
+                    Greeting("Curso Gitgit log" +
+                            "Hub")
                 }
             }
         }
